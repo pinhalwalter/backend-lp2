9be6b947a7bb1149a15742939dc88009ecd39fad
renatofernandosilvagoncalves <renatogoncalves@unoeste.edu.br>
2024-10-14 09:01:06 -0300

    iniciando o repositório

---
+++ Persistencia/Conexao.js
@@ -0,0 +1,25 @@
+import mysql from 'mysql2/promise';
+
+//Lembre-se, nossa aplicação neste momento estará se comunicando com outra aplicação.
+//Desse modo, nossa aplicação não tem controle sobre a outra.
+//O que exige uma comunicação assíncrona.
+export default async function conectar(){
+    
+    if (global.poolConexoes){
+        //retorna do pool uma conexão
+        return await poolConexoes.getConnection();
+    }
+    else{
+        global.poolConexoes = await mysql.createPool({
+            "host":'localhost',
+            "port":3306,
+            "database":"loja",
+            "user":"root",
+            "password":"",
+            "connectTimeout":60000,
+            "waitForConnections":true,
+            "queueLimit":20
+        });
+        return await global.poolConexoes.getConnection();
+    }
+}
